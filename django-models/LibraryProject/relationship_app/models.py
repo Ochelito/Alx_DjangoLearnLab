86df8c25073530110369d96883d248023ac062e5
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-20 11:55:33 -0700

    implemented custom permissions in django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,6 +8,13 @@ from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
     
     def __str__(self):
         return self.name
