35f1c18b295d46c8dd942c1c6fa908ae1f57cda9
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-19 21:16:48 -0700

    Implemented advanced  model relationships in my django app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -38,13 +38,12 @@ def get_librarian_for_library(library_name):
 
     try:
         library = Library.objects.get(name=library_name)
-
-        librarian = library.librarians  #Reverse relation from the OneToOneField
+        librarian = Library.objects.get(library=library)
     
-        print(f"Librarian for {library_name}: {librarian.name}")
+        print(f"Librarian for '{library_name}': {librarian.name}")
 
     except Library.DoesNotExist:
-        print(f"No library found with the name {library_name}")
+        print(f"Library '{library_name}' not found")
 
     except Librarian.DoesNotExist:
         print(f"No librarian assigned to {library_name}")
