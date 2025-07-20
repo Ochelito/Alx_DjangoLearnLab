1d93064b3fbc26833ac6f1c6c9444cba65ca9ed9
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-20 08:54:26 -0700

    Updated apps url to import views correctly

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register
+from . import views
+#from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
-    path('', list_books, name='list_books'),
-    path('relationship_app/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('', views.list_books, name='list_books'),
+    path('relationship_app/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register')
+    path('register/', views.register, name='register')
 ]
