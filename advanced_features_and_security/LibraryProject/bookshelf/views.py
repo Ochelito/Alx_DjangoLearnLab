9175e187a64f5dc908513db0d2a039d070b51a62
Idoko Augustine <linkwithaugustine@gmail.com>
2025-07-27 05:19:24 -0700

    implemented security best practices in django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 # View list of books
 @permission_required('books.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'books/book_list.html', {'books': books})
+    return render(request, 'templates/book_list.html', {'books': books})
 
 
 # Create a new book
 @permission_required('books.can_create', raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm()
-    return render(request, 'books/book_form.html', {'form': form})
+        form = ExampleForm()
+    return render(request, 'templates/form_example.html', {'form': form})
 
 
 # Edit an existing book
@@ -28,13 +28,13 @@ def book_create(request):
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm(instance=book)
-    return render(request, 'books/book_form.html', {'form': form})
+        form = ExampleForm(instance=book)
+    return render(request, 'templates/form_example.html', {'form': form})
 
 
 # Delete a book
@@ -44,4 +44,4 @@ def book_delete(request, pk):
     if request.method == 'POST':
         book.delete()
         return redirect('book_list')
-    return render(request, 'books/book_confirm_delete.html', {'book': book})
+    return render(request, 'templates/book_confirm_delete.html', {'book': book})
