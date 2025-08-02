f7ef7dac0f5350586a7e5aa0de5bc01e8049b3b9
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-01 20:10:06 -0700

    Added retrieve book list API endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -8,4 +9,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,11 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import BookList
-
-router = DefaultRouter()
-router.register(r'books', BookList, basename='book')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
