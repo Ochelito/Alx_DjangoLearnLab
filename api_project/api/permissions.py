f6addd583218524922071f102039aa5a953e754d
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-01 22:16:01 -0700

    implemented Token authentication and custom permission

---
+++ api_project/api/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        
+        # Allow safe methods like GET, HEAD, OPTIONS
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import BookViewSet, BookList
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -8,4 +10,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
