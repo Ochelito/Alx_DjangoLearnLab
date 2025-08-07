3d405e4073a9f3696e547c4acc98a2dc9105719e
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-06 19:41:19 -0700

    created django project with custom serializer in DRF

---
+++ advanced_api_project/api/seriaizers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import date
+
+# Serializer for the Book model
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'  # Includes all fields of the Book model in the serialization
+
+    # Custom validation method for the 'publication_year' field
+    def validate_publication_year(self, value):
+        current_year = date.today().year
+        if value > current_year:
+            #Prevents future years from being used as the publication year
+            raise serializers.ValidationError("Publication year cannot be in the future.")
+        return value  #Returns the valid year when inputted
+
+# Serializer for the Author model
+class AuthorSerializer(serializers.ModelSerializer):
+    #Nests related books inside the Author serializer, read-only
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['name', 'books']  #Serializes only the author's name and related books
+        # This code registers the Author and Book models with the Django admin site,
+        # allowing them to be managed through the admin interface.
---
+++ advanced_api_project/api/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import BookSerializer
+
+# Create your views here.
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.DetailAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(generics.DetailAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(generics.DeleteAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
