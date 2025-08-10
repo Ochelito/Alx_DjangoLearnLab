dc8d089bb73e75e982dd59fe25a293c647ae0226
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-10 08:21:50 -0700

    added search, filtering and ordering functionality to my django project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import BasePermission 
 from datetime import datetime
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework import filters
 
 
@@ -22,10 +22,10 @@ class IsAuthorOrReadOnly(BasePermission):
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthorOrReadOnly]  # Allow unauthenticated users to view the list of books
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]  # Allow unauthenticated users to view the list of books
 
      # Enable filtering
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author__name']  # Enable search by title and author's name
     ordering_fields = ['title', 'publication_year']  # Allow ordering by publication year and title
@@ -35,13 +35,13 @@ class BookListView(generics.ListAPIView):
 class BookDetailView(generics.DetailAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthorOrReadOnly]  # Allow unauthenticated users to view book details
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]  # Allow unauthenticated users to view book details
 
 # Create a new book - accessible to authenticated users only
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]   # Allow only logged in users to create a book; author set automatically
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]   # Allow only logged in users to create a book; author set automatically
 
     def perform_create(self, serializer): # Saves the book with the current user as the author
         serializer.save(author=self.request.user) # 
@@ -50,7 +50,7 @@ class BookCreateView(generics.CreateAPIView):
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]  # Allow only logged in users and authors to update a book
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]  # Allow only logged in users and authors to update a book
 
     def perform_update(self, serializer): # Saves the book with the current user as the author
         serializer.save() # Updates the book with the current user as the author
@@ -64,4 +64,4 @@ class BookUpdateView(generics.UpdateAPIView):
 class BookDeleteView(generics.DeleteAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]  # Allow only logged in users and authors to delete a book
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]  # Allow only logged in users and authors to delete a book
