13ceca908ca616565598df9e7e503f2e461b4a16
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-17 07:50:56 -0700

    created Blog post management features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, ProfileView, HomeView, CustomLogoutView, CustomLoginView 
+from .views import UserRegisterView, ProfileView, HomeView, CustomLogoutView, CustomLoginView, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -7,5 +7,10 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),  
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import CustomUserCreationForm, ProfileForm, UserEditForm, UserUpdateForm
+from .forms import CustomUserCreationForm, ProfileForm, UserEditForm, UserUpdateForm, PostForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views import View
-from django.contrib.auth.mixins import LoginRequiredMixin 
-from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView, UpdateView, ListView, DetailView, CreateView, DeleteView
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
+from .models import Post
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -68,3 +69,43 @@ class ProfileView(LoginRequiredMixin, TemplateView):
             'user_form': user_form,
             'profile_form': profile_form
         })
+    
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# View single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Edit a post (only by author)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
+
+# Delete a post (only by author)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        return self.get_object().author == self.request.user
