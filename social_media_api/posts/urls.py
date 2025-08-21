2ebf6cdc50e717bea72d9b9deeba121d813cd640
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-20 20:14:46 -0700

    implemented notification and likes functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,6 +9,6 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', UserFeedView.as_view(), name='user-feed'),
-    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
